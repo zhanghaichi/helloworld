5e8316defc434e3ef7b50b100dc3c7ce0df24f26
zhanghaichi <zhanghaichi@gmail.com>
2024-03-23 15:16:08 +0900

    hellojava

---
+++ hellojava.java
@@ -0,0 +1,15 @@
+import static java.util.Comparator.*;
+
+import java.util.ArrayList;
+
+public class Mypro {
+
+	public static void main(String[] args) {
+
+system.out.println"hello world");
+system.out.println"1");
+system.out.println"hello world");
+system.out.println"2");
+}
+
+}
